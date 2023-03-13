ac3f86252d4318f39c4343a8ad09376f1c95a0cb
Rodrigo Luís Brasil Filho <125308019+lestofus@users.noreply.github.com>
2023-03-13 14:03:34 -0300

    Add files via upload

---
+++ Logica de programação/Objeto.js
@@ -0,0 +1,25 @@
+//objeto "Pedro"
+
+let pedro = {
+    nome: "Pedro Nieto",
+    Idade: 17,
+    Trabalhando: true
+}
+
+
+
+/*função com Object.assign
+function copiarObjectAssign(nomeDoObjeto, propriedadeDoObjeto) {
+    const copia = Object.assign {{}, nomeDoObjeto}
+    delete copia[propriedadeDoObjeto]
+    return copia
+}
+*/
+
+//função com spread
+function copiarLista(nomeDoObjeto, propriedadeDoObjeto) {
+    const copia = { ...nomeDoObjeto }
+    delete copia[propriedadeDoObjeto]
+    return copia
+}
+console.log(copiarLista(pedro, 'idade'))
---
+++ Logica de programação/rep++.js
@@ -0,0 +1,11 @@
+function mais(repMais)
+{
+    let mais = ""
+
+    for(let i = 0; i < repMais ; i++)
+    {
+        mais += "+"
+    }
+    console.log(mais)
+}
+mais(3)
